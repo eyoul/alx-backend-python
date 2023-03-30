1dcf988fadc6de8d9bc4caaec1c59c64e4f51327
eyoul <eyoulshimeles2@gmail.com>
2023-03-30 18:00:53 +0000

    update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,4 +1,6 @@
 from typing import Iterable, Sequence, List, Tuple
+"""unction’s parameters and return values with the appropriate types"""
+
 
 def element_length(lst: Iterable[Sequence]
                    ) -> List[Tuple[Sequence, int]]:
