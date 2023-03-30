1300bbdbe7e5e877dbe8ac7dcbd0523249cd1833
eyoul <eyoulshimeles2@gmail.com>
2023-03-30 16:58:11 +0000

    update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -2,6 +2,7 @@
 """type-annotated function floor which takes a float n as argument"""
 import math
 
+
 def floor(n: float) -> int:
     """Returns the floor of the float."""
     return math.floor(n)
