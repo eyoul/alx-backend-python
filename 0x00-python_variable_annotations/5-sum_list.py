6d8a2689924f2e6f2fd86d1a50414c0f9a752844
eyoul <eyoulshimeles2@gmail.com>
2023-03-30 17:12:20 +0000

    update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+"""type-annotated function sum_list which takes a list input_list"""
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     if input_list in None:
