5bd3c9509f4ab9c75914455469dacce60b5a9413
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-04-03 11:03:06 -0700

    update 4-tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-"""execute multiple coroutines at the same time with async"""
+"""Take the code from wait_n and alter it into a new function task_wait_n"""
 
 import asyncio
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def waitask_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    2 int arguments (in this order): n and max_delay
-    async routine called wait_n
+    it into a new function task_wait_n
+    identical to wait_n except task_wait_random
     """
 
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
